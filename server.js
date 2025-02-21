6bb2cf7b487d64aa9ee0ffbeecdb73d5c286aca9
Ravikharel <ravikharel77@gmail.com>
2025-02-21 08:36:57 +0000

    last change made to the git

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Home Route
 app.get('/', (req, res) => {
-    res.send('<h1>Jenkins Deployed This App 🚀</h1>');
+    res.send(`
+        <h1>Jenkins Deployed This App 🚀</h1>
+        <p>Welcome! Try visiting <a href="/hello/Ravi">/hello/YourName</a></p>
+    `);
 });
 
+// Dynamic Route
+app.get('/hello/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`<h1>Hello, ${name}! 👋</h1><p>Jenkins has deployed this successfully.</p>`);
+});
+
+// Start Server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
